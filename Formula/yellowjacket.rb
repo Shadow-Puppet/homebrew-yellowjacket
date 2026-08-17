69cc7cbdad60438b8ec31f5f7f749e4b13a0ddf5
yellowjacket-ci <yj@yellowjacket.app>
2026-08-17 15:27:29 +0000

    yellowjacket 1.6.0

--- Formula/yellowjacket.rb
+++ Formula/yellowjacket.rb
@@ -15,9 +15,9 @@
 class Yellowjacket < Formula
   desc "Cross-platform desktop music player — local library, MusicBrainz explore & auto-tag"
   homepage "https://git.ljones.me/yonlu/yellowjacket"
-  version "1.5.0"
+  version "1.6.0"
   url "https://git.ljones.me/yonlu/yellowjacket/archive/v#{version}.tar.gz"
-  sha256 "2a8efb0b3584148e9ca661c0b1ef310a0da98c5fddc8a77c0bb882201b6b1876"
+  sha256 "6df181cebd1698d46a29af1fd028d8d8b747236c3be2f71b780ca1d107ae3958"
   license :cannot_represent # custom license — see repository
 
   head "https://git.ljones.me/yonlu/yellowjacket.git", branch: "main"
